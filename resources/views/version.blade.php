a788928e718f6796ef97874432a03feb4b03866a
Senna2112 <raff0001@hz.nl>
2021-06-15 15:18:27 +0200

    version blade

---
+++ resources/views/version.blade.php
@@ -0,0 +1,13 @@
+<?php
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <p>Version: 1</p>
+            </div>
+        </div>
+    </div>
+@endsection
